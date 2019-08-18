6bf13d31cd16e54ec70eded895f3a298e7436736
kasper Tvede <kasper@webmasteren.eu>
2019-08-18 11:16:32 +0200

    mark "nothing" functions. and minor things

---
+++ src/main/kotlin/csense/idea/kotlin/checked/exceptions/annotator/ThrowsFunctionAnnotator.kt
@@ -0,0 +1,31 @@
+package csense.idea.kotlin.checked.exceptions.annotator
+
+import com.intellij.lang.annotation.*
+import com.intellij.openapi.util.*
+import com.intellij.psi.*
+import csense.idea.kotlin.checked.exceptions.bll.*
+import csense.idea.kotlin.checked.exceptions.intentionAction.*
+import org.jetbrains.kotlin.name.*
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.*
+import org.jetbrains.kotlin.resolve.descriptorUtil.*
+import org.jetbrains.kotlin.types.typeUtil.*
+
+class ThrowsFunctionAnnotator : Annotator {
+
+    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+        val exp = element as? KtCallExpression ?: return
+        val resultingDescriptor = exp.resolveToCall()?.resultingDescriptor
+        val isNothing = resultingDescriptor?.returnType?.isNothing()
+        if (isNothing != true) {
+            return
+        }
+        //per spec: https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/-nothing.html
+        // for example, if a function has the return type of Nothing, it means that it never returns (always throws an exception).
+        val range = TextRange(element.getTextRange().startOffset,
+                element.getTextRange().endOffset)
+        holder.createWarningAnnotation(
+                range,
+                "Throws inside of function")
+    }
+}
--- src/main/kotlin/csense/idea/kotlin/checked/exceptions/bll/constants.kt
+++ src/main/kotlin/csense/idea/kotlin/checked/exceptions/bll/constants.kt
@@ -1,2 +1,7 @@
 package csense.idea.kotlin.checked.exceptions.bll
 
+import org.jetbrains.kotlin.name.*
+
+const val kotlinMainExceptionFqName = "Kotlin.Exception"
+val kotlinMainExceptionFq = FqName(kotlinMainExceptionFqName)
+const val kotlinThrowsText = "Throws"
