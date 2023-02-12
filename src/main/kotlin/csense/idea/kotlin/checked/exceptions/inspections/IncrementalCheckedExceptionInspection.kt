d5824e71482f9a0e5cd68d563d894e5d13d7d6a6
Kasper T <kasper@webmasteren.eu>
2023-02-12 15:35:03 +0100

    Update IncrementalCheckedExceptionInspection.kt

--- src/main/kotlin/csense/idea/kotlin/checked/exceptions/inspections/IncrementalCheckedExceptionInspection.kt
+++ src/main/kotlin/csense/idea/kotlin/checked/exceptions/inspections/IncrementalCheckedExceptionInspection.kt
@@ -12,13 +12,11 @@ import csense.idea.base.bll.psiWrapper.`class`.operations.*
 import csense.idea.base.bll.psiWrapper.function.*
 import csense.idea.base.bll.psiWrapper.function.operations.*
 import csense.idea.base.visitors.*
-//import csense.idea.kotlin.checked.exceptions.annotator.*
 import csense.idea.kotlin.checked.exceptions.bll.*
 import csense.idea.kotlin.checked.exceptions.bll.callthough.*
 import csense.idea.kotlin.checked.exceptions.bll.ignore.*
 import csense.idea.kotlin.checked.exceptions.builtin.callthough.*
 import csense.idea.kotlin.checked.exceptions.builtin.operations.*
-//import csense.idea.kotlin.checked.exceptions.callthough.*
 import csense.idea.kotlin.checked.exceptions.settings.*
 import csense.kotlin.extensions.*
 import csense.kotlin.extensions.collections.*
