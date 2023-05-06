c446f94282cb19844b5745ba236f850779ca0e25
Kasper T <kasper@webmasteren.eu>
2023-05-06 12:51:35 +0200

    bump and clean build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
-    //https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "1.13.2"
+//https://github.com/JetBrains/gradle-intellij-plugin
+    id("org.jetbrains.intellij") version "1.13.3"
     //https://github.com/JetBrains/kotlin
-    kotlin("jvm") version "1.8.20-RC2"
-    //https://github.com/jeremylong/dependency-check-gradle/releases
+    kotlin("jvm") version "1.8.21"
+    //https://jeremylong.github.io/DependencyCheck/
     id("org.owasp.dependencycheck") version "8.2.1"
 }
+
+val javaVersion = "11"
 
 group = "csense-idea"
 version = "2.0.0"
@@ -26,17 +28,23 @@ repositories {
     }
 }
 
-dependencies {
-    implementation("csense.kotlin:csense-kotlin-jvm:0.0.60")
-    implementation("csense.kotlin:csense-kotlin-annotations-jvm:0.0.63")
-    implementation("csense.idea.base:csense-idea-base:0.1.60")
 
-    testImplementation("junit:junit:4.13.2")
+dependencies {
+    //https://github.com/csense-oss/csense-kotlin
+    implementation("csense.kotlin:csense-kotlin-jvm:0.0.60")
+    //https://github.com/csense-oss/csense-kotlin-annotations
+    implementation("csense.kotlin:csense-kotlin-annotations-jvm:0.0.63")
+    //https://github.com/csense-oss/idea-kotlin-shared-base
+    implementation("csense.idea.base:csense-idea-base:0.1.60")
+    //https://github.com/Kotlin/kotlinx.serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    //https://github.com/Kotlin/kotlinx.coroutines
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+    //https://github.com/csense-oss/csense-kotlin-test
     testImplementation("csense.kotlin:csense-kotlin-tests:0.0.60")
+    //https://github.com/csense-oss/csense-oss-idea-kotlin-shared-test
     testImplementation("csense.idea.test:csense-idea-test:0.3.0")
 }
-
 
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
     changeNotes.set(
@@ -50,12 +58,19 @@ tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml
 tasks.getByName("check").dependsOn("dependencyCheckAnalyze")
 
 tasks {
+
+    withType<JavaCompile> {
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+    }
+
     compileKotlin {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = javaVersion
     }
     compileTestKotlin {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = javaVersion
     }
+
     test {
         testLogging {
             showExceptions = true
@@ -63,17 +78,14 @@ tasks {
             exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
         }
     }
-    buildSearchableOptions {
-        enabled = true
-    }
-    runIde {
+        runIde {
 //        ideDir.set(file("/home/kasper/.local/share/JetBrains/Toolbox/apps/AndroidStudio/ch-0/211.7628.21.2111.8139111/"))
     }
 }
 
 kotlin {
     jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
+        languageVersion.set(JavaLanguageVersion.of(javaVersion))
     }
 }
 
